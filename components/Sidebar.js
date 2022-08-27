cbb1027d37ad85faa60dcaa045f67ca05b05c4b7
fkadeal <96823972+F21deal@users.noreply.github.com>
2022-08-26 20:37:58 -0800

    fix icon , and sm crean size for sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -13,8 +13,8 @@ import {
 function Sidebar() {
     return(
         <div className="hidden sm:flex flex-col items-center  xl:items-start xl:w-[340] p-2 fixed h-full">
-            <div className="flex item-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
-        <Image src="https://rb.gy/ogau5a" width={30} height={30}/>
+            <div className="flex item-center justify-center w-14 h-14   p-0 xl:ml-24">
+        <Image src="https://rb.gy/ogau5a" width={40} height={30}/>
             </div>
             <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
                 <SideBarLink text="Home" Icon={HomeIcon} active />
