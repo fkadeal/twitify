a09d19cf027d09ea852ab6c4febe319b6e720e34
fkadeal <96823972+F21deal@users.noreply.github.com>
2022-09-25 10:30:43 -0800

    ui :fix

--- components/Feed.js
+++ components/Feed.js
@@ -1,16 +1,21 @@
-import { SparklesIcon } from "@heroicons/react/solid";
+import { SparklesIcon } from '@heroicons/react/solid'
+import Input from './Input'
 
-function Feed(){
-    return <>
-    <div className="text-gray-200 flex-grow border-l border-r border-gray-700 max-w-2x1 sm:ml-[73px] xl:ml-[310px]">
-        <div className="flex mx-1 text-gray-50 py-2 px-3 items-center sticky top-0 z-50 border-b border-gray-700 sm:justify-between">
-            <h2 className="text-lg sm:text-xl font-bold">Home</h2>
-            <div>
-                <SparklesIcon className="h-5 text-white" />
-            </div>
+function Feed() {
+  return (
+    <>
+      <div className="max-w-2x1 flex-grow border-l border-r border-gray-700 text-gray-200 sm:ml-[73px] xl:ml-[310px]">
+        <div className="sticky top-0 z-50 mx-1 flex items-center border-b border-gray-700 py-2 px-3 text-gray-50 sm:justify-between">
+          <h2 className="text-lg font-bold sm:text-xl">Home</h2>
+          <div>
+            <SparklesIcon className="h-5 text-white" />
+          </div>
         </div>
-    </div>
+
+        <Input />
+      </div>
     </>
+  )
 }
 
-export default Feed;
+export default Feed
