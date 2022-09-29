3d2cbde5d79c320278e8219648c4054d73d258e2
fkadeal <96823972+F21deal@users.noreply.github.com>
2022-09-29 07:05:49 -0800

    wip: add session and display username: but needs some trim for the ui

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,6 @@
 import SideBarLink from "./SideBarLink";
 import Image from "next/image";
+import { useSession ,signOut } from "next-auth/react";
 import {DotsHorizontalIcon, HomeIcon} from "@heroicons/react/solid";
 import {
     HashtagIcon,
@@ -11,6 +12,7 @@ import {
     DotsCircleHorizontalIcon
 } from "@heroicons/react/outline";
 function Sidebar() {
+    const {data: session}= useSession()
     return(
         <div className="hidden sm:flex flex-col items-center  xl:items-start xl:w-[340] p-2 fixed h-full">
             <div className="flex item-center justify-center w-14 h-14   p-0 xl:ml-24">
@@ -29,12 +31,12 @@ function Sidebar() {
 
             <button className="hoverAnimation shadow-md text-ls font-bold hidden xl:inline ml-auto bg-blue-600 text-white text-lg rounded-full w-2/3 h-[52px] hover:bg-blue-800  xl:ml-auto ">Tweet</button>
             
-            <div className="mt-auto hoverAnimation flex items-center justify-center xl:ml-auto xl:-mr-5 text-gray-100">
-                <img src="https://lh3.googleusercontent.com/ogw/AOh-ky0oCERlae2OJ42TKZ2baGTvEZJEbLcP6Kb9kqvnSg=s64-c-mo" 
+            <div className="mt-auto hoverAnimation flex items-center justify-center xl:ml-auto xl:-mr-5 text-gray-100" onClick={()=> signOut()}>
+                <img src={session.user.image} 
                 alt="" className="h-10 w-10 rounded-full xl:mr-2.5" />
                 <div className="hidden xl:inline leading-5">
-                    <h4 className="font-bold">{'fkadeal mat'}</h4>
-                    <p className="text-gray-400">{'@fkadealkl'} </p>
+                    <h4 className="font-bold">{session.user.name}</h4>
+                    <p className="text-gray-400">@{session.user.tag} </p>
                 </div>
             <DotsHorizontalIcon className="h-5 hidden xl:inline ml-5" />
             </div> 
